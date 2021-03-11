3d52278acf93c91b9b15a2a4758ede163dd47974
abbos2101 <abbos2101@gmail.com>
2021-03-11 17:26:07 +0500

    build web qo'shildi

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_resume_app/data/hive/hive.dart';
 import 'package:flutter_resume_app/di/locator.dart';
@@ -13,36 +14,59 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  final HiveDB hiveDB = locator.get<HiveDB>();
-
-  @override
-  void initState() {
-    nextScreen();
-    super.initState();
-  }
-
-  Future<void> nextScreen() async {
-    await Future.delayed(Duration(seconds: 3));
-    if (await hiveDB.getToken() == null)
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => LoginScreen.screen()),
-      );
-    else
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => MainScreen.screen()),
-      );
-  }
+  final String token = "45425426-1363-4ee3-9056-7716b5d91583";
+  bool isLoading = false;
+  String slogan = "";
+  String auth = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.asset(
-        "assets/img_splash_background.png",
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              setState(() => isLoading = true);
+              try {
+                Response response = await Dio().get(
+                  "http://185.16.40.113:8080/api/slogan/random",
+                );
+                slogan = "${response.data}";
+                setState(() => isLoading = false);
+              } catch (e) {
+                slogan = "$e";
+                setState(() => isLoading = false);
+              }
+            },
+          ),
+          SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: () async {
+              setState(() => isLoading = true);
+              try {
+                Response response = await Dio().post(
+                  "http://185.16.40.113:8080/api/auth/login",
+                  data: {"username": "a", "password": "1"},
+                );
+                auth = "${response.data}";
+                setState(() => isLoading = false);
+              } catch (e) {
+                auth = "$e";
+                setState(() => isLoading = false);
+              }
+            },
+          ),
+        ],
+      ),
+      body: Container(
         width: double.infinity,
         height: double.infinity,
-        fit: BoxFit.fill,
+        alignment: Alignment.center,
+        color: Colors.white,
+        child: isLoading == true
+            ? CircularProgressIndicator()
+            : Text("Slogan = $slogan\n\n*****\n\nAuth = $auth"),
       ),
     );
   }
